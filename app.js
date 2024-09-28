bb40da165f6a92fe9fabc93cc3e23a27285e4b4b
shhheshable <134524454+shhheshable@users.noreply.github.com>
2024-09-29 01:56:50 +0800

    commit On branch LanceEspina Your branch is up to date with 'origin/LanceEspina'.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use()
-app.use()
-app.set()
+app.set('view engine', 'ejs');
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.use('/', authRoutes);
+
+
+const PORT = process.env.PORT || 1000;
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});

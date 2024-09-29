b8fe62c99ae6b07aad8afff89167f130f751a1f6
shhheshable <134524454+shhheshable@users.noreply.github.com>
2024-09-29 15:02:56 +0800

    commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const authRoutes = require('../controllers/authController');
+const authController = require('../controllers/authController');
+
+// Route to display the login page
+router.get('/login', (req, res) => {
+    res.render('user/login'); // Path to the EJS file
+});
+
+// Route for the root URL
+router.get('/', (req, res) => {
+    res.redirect('/login'); // Redirect to the login page
+});
+
+// You can add more routes here for authentication logic, etc.
+
+module.exports = router;

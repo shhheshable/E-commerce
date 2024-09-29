ff58f0bc5d5848ec1aa2c022279213b0c7d5d5a0
shhheshable <134524454+shhheshable@users.noreply.github.com>
2024-09-29 15:58:08 +0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const admin = require('./routes/admin');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up routes
-app.use('/', admin);
+app.use('/', authRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {

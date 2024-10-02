add1b318cd3b8fa8186b98031965a1540ba80c9d
christj831 <sijhayortega12@gmail.com>
2024-10-03 02:03:35 +0800

    commit

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController');
+const homeController = require('../controllers/authController');
 
 // Route to display the login page
 router.get('/home', (req, res) => {
     res.render('user/home'); // Path to the EJS file
+});
+
+router.post('/login', homeController.logs);
+
+router.get('/blala', (req, res) => {
+    const userToken = req.session.userToken; // Access token from session
+    res.render('user/blala', { token: userToken }); // Pass token to the view
 });
 
 module.exports = router;
